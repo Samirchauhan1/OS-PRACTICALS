59257332a3b75932180dafb4596ea9c42aa9480b
Samirchauhan1 <chauhansamir560@gmail.com>
2026-03-20 10:49:52 +0530

    Added Code

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter your name:"
+read name
+
+echo "Enter your age:"
+read age
+
+echo "-----------------------------"
+echo "Hello, $name"
+echo "You are $age years old"
+echo "-----------------------------"
